0927e03de88a2b470ff5d389ba2e4cb6520bd48f
Chris Hayes <v-chrhayes@microsoft.com>
2025-04-25 13:44:01 -0700

    inline ifs to ternary

--- index.js
+++ index.js
@@ -2,114 +2,101 @@ const canvas = document.getElementById("board");
 const ctx = canvas.getContext("2d");
 
 let state = {
-  cards: [
-    { x: 20, y: 20, height: 20, width: 20, color: "green", selected: false },
-  ],
-  dzs: [{ x: 100, y: 20, width: 100, height: 100, over: false }],
+    cards: [
+        { x: 20, y: 20, height: 20, width: 20, color: "green", selected: false },
+    ],
+    dzs: [{ x: 100, y: 20, width: 100, height: 100, over: false }],
 };
 
 function isPointInsideRectangle(point, rect) {
-  const { x, y } = point;
-  const { x: rectX, y: rectY, width, height } = rect;
+    const { x, y } = point;
+    const { x: rectX, y: rectY, width, height } = rect;
 
-  return x >= rectX && x <= rectX + width && 
-         y >= rectY && y <= rectY + height;
+    return x >= rectX && x <= rectX + width &&
+        y >= rectY && y <= rectY + height;
 }
 
 function draw() {
-  const statejson = document.getElementById("state");
-  statejson.innerText = JSON.stringify(state, null, 2);
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "white";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  for (const dz of state.dzs) {
-    if (dz.over) {
-      ctx.fillStyle = "red";
-    } else {
-      ctx.fillStyle = "silver";
+    const statejson = document.getElementById("state");
+    statejson.innerText = JSON.stringify(state, null, 2);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    for (const dz of state.dzs) {
+        ctx.fillStyle = dz.over ? "red" : "silver";
+        ctx.fillRect(dz.x, dz.y, dz.width, dz.height);
     }
-    ctx.fillRect(dz.x, dz.y, dz.width, dz.height);
-  }
 
-  for (const card of state.cards) {
-    if (card.selected) {
-      ctx.fillStyle = "blue";
-    } else {
-      ctx.fillStyle = "green";
+    for (const card of state.cards) {
+        ctx.fillStyle = card.selected ? "blue" : "green";
+        ctx.fillRect(card.x, card.y, card.width, card.height);
     }
-    ctx.fillRect(card.x, card.y, card.width, card.height);
-  }
 }
 
 function getMousePos(evt) {
-  var rect = canvas.getBoundingClientRect();
-  return {
-    x: evt.clientX - rect.left,
-    y: evt.clientY - rect.top,
-  };
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top,
+    };
 }
 
 let isover = false;
 let isdown = false;
 
 canvas.addEventListener("mouseover", function (event) {
-  isover = true;
+    isover = true;
 });
 
 canvas.addEventListener("mouseleave", function (event) {
-  isover = false;
+    isover = false;
 });
 
 canvas.addEventListener("mousedown", function (event) {
-  isdown = true;
-  const canevent = getMousePos(event);
-  for (const card of state.cards) {
-    if (isPointInsideRectangle(canevent, card)) {
-      console.log("card selected");
-      card.selected = true;
+    isdown = true;
+    const canevent = getMousePos(event);
+    for (const card of state.cards) {
+        card.selected = isPointInsideRectangle(canevent, card);
     }
-  }
-  draw();
+    draw();
 });
 
 canvas.addEventListener("mouseup", function (event) {
-  isdown = false;
-  for (const card of state.cards) {
-    card.selected = false;
-  }
-  for (const dz of state.dzs){
-    dz.over = false;
-  }
-  draw();
+    isdown = false;
+    for (const card of state.cards) {
+        card.selected = false;
+    }
+    for (const dz of state.dzs) {
+        dz.over = false;
+    }
+    draw();
 });
 
 canvas.addEventListener("mousemove", function (event) {
-  if (isover) {
-    const x = document.getElementById("x");
-    const y = document.getElementById("y");
-    x.innerText = `(x:${event.x})`;
-    y.innerText = `(y:${event.y})`;
-  }
-  const canevent = getMousePos(event);
-  if (isover && isdown) {
-    let selected = false;
-    for (const card of state.cards) {
-      if (card.selected) {
-        selected = true;
-        card.x = Math.floor(canevent.x - card.width / 2);
-        card.y = Math.floor(canevent.y - card.height / 2);
-        draw();
-        break;
-      }
+    if (isover) {
+        const x = document.getElementById("x");
+        const y = document.getElementById("y");
+        x.innerText = `(x:${event.x})`;
+        y.innerText = `(y:${event.y})`;
     }
-    if (selected) {
-      for (const dz of state.dzs) {
-        if (isPointInsideRectangle(canevent, dz)) {
-          dz.over = true;
-        } else { dz.over = false; }
-      }
+    const canevent = getMousePos(event);
+    if (isover && isdown) {
+        let selected = false;
+        for (const card of state.cards) {
+            if (card.selected) {
+                selected = true;
+                card.x = Math.floor(canevent.x - card.width / 2);
+                card.y = Math.floor(canevent.y - card.height / 2);
+                draw();
+                break;
+            }
+        }
+        if (selected) {
+            for (const dz of state.dzs) {
+                dz.over = isPointInsideRectangle(canevent, dz);
+            }
+        }
     }
-  }
 });
 
 draw();
